b290fa7b6aac083ffe0eed0449710857205851ab
SAYAN DAS <poke19962008@gmail.com>
2016-12-01 21:48:02 +0530

    added song loaders

---
+++ main.py
@@ -0,0 +1,47 @@
+import librosa
+import pickle, json, os
+
+class mash:
+
+    def __init__(self, json_, cached=False):
+        self.sr = 22050  # new Sampling Rate for the audio files
+
+        self.songs = json_
+        self.Yin = []
+        self.Yout = []
+        self.load(cached=cached)
+
+    def load(self, cached=True):
+        for song in self.songs:
+            if os.path.exists("cache/%s.pkl"%song['name']):
+                print "\nLoading", song['name'], "from cache"
+                with open("cache/%s.pkl"%song['name'], 'rb') as f:
+                    if song['mixin']:
+                        self.Yin = pickle.load(f)
+                    else:
+                        self.Yout.append(pickle.load(f))
+                continue
+
+            print "\nLoading", song['name']
+            y, sr = librosa.load(song['path'], sr=self.sr)
+            if song['mixin']:
+                self.Yin = y
+            else:
+                self.Yout.append(y)
+            print "[SUCCESS] Loaded", song['name']
+
+            if cached:
+                try:
+                    with open('cache/%s.pkl'%song['name'], 'wb') as f:
+                        pickle.dump(y, f)
+                        print "[SUCCESS] Cached", song['name']
+                except Exception as e:
+                    print "[FAILED] Caching", song['name']
+                    print e
+
+
+
+if __name__ == '__main__':
+    with open('songs.json', 'r') as f:
+        j = json.loads(f.read())
+        obj = mash(j, cached=True)
